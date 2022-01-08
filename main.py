12777511c5ad076c1603b40319d211b19ba70bba
klaudiii <klaudiastp@gmail.com>
2022-01-08 11:32:30 +0100

    delete comments in main

--- main.py
+++ main.py
@@ -28,39 +28,11 @@ num_best = 2
 quality_index_type = 0
 
 if __name__ == "__main__":
+
+
     # tests:
     # print(f'test covers(): {test_covers()}')
     # print(f'test delete_non_general(): {test_delete_non_general()}')
-
-    # when working on a project, execute this once to create a tic-tac-toe or spect table from raw data
-    # save_ttt_table()
-    # save_spect_train_table()
-
-
-    #aq
-
-    # df = create_prepared_table(dataset)
-    # # df = create_test_table()
-    # print(df)
-    #
-    # if not rules_from_file:
-    #     rules = induce_rules(df.loc[:split], num_best, quality_index_type)
-    #     save_rules_to_file(rules, 'rules')
-    # else:
-    #     rules = load_rules_from_file(f'rules_{dataset}')
-    # print(f'rules number: {len(rules)}\n{rules}')
-    #
-    # # load test data set for spect data
-    # if 'spect' == dataset:
-    #     df = load_spect_test_table()
-    #     split = 0
-    #
-    # df_pred = predict_table(rules, df.loc[split:], 'pred')
-    #
-    # print(df_pred)
-    # print(f'Accuracy: {np.round(np.sum(df_pred.iloc[:, -2] == df_pred.iloc[:, -1]) / df_pred.shape[0] * 100, 3)}%')
-
-
 
     #cn2
 
